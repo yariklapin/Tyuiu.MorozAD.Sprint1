ce8f16b1ca27eacf554541e6b1716b8cbec16899
Tyuiu.MorozAD@outlook.com <ggllaa5566q@gmail.com>
2023-09-19 17:19:22 +0500

    Add project files.

---
+++ Tyuiu.MorozAD.Sprint1.Task1/Tyuiu.MorozAD.Sprint1.Task1.cpp
@@ -0,0 +1,36 @@
+﻿// Tyuiu.MorozAD.Sprint1.Task1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    int b;
+    int c;
+    int d;
+
+    cout <<"Send number: \n";
+    int x;
+    cin >> x;
+    if (x >= 100 && x <= 999)
+    {
+         b = (x / 10)%10;
+         c = x % 10;
+         a = x / 100;
+         d = (b + a + c);
+
+    }
+    else
+        cout << "it's not number with 3 digits \n";
+
+    if (d>=10 && d<= 100) 
+    {
+        cout << "Summ: " << d;
+    }
+    else
+        cout << "ne dvuxznacnoe";
+    
+
+}
+
