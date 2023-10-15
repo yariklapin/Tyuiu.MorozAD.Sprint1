7b4f3eb5082dd7772fd31c710f32375f6a0834e4
Tyuiu.MorozAD@outlook.com <ggllaa5566q@gmail.com>
2023-10-15 21:50:04 +0500

    Task2

--- Tyuiu.MorozAD.Sprint1.Task1.V12/Program.cs
+++ Tyuiu.MorozAD.Sprint1.Task1.V12/Program.cs
@@ -3,11 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tyuiu.MorozAD.Sprint0.Task1.V0.Lib;
+using Tyuiu.MorozAD.Sprint1.Task2.V12.Lib;
 
-
-
-namespace Tyuiu.MorozAD.Sprint1.Task0.V0
+namespace Tyuiu.MorozAD.Sprint1.Task1.V12
 {
     class Program
     {
@@ -19,25 +17,34 @@ namespace Tyuiu.MorozAD.Sprint1.Task0.V0
             Console.WriteLine("************************************************************************");
             Console.WriteLine("* Спринт #1                                                            *");
             Console.WriteLine("* Тема: Базовые навыки работы С#                                       *");
-            Console.WriteLine("* Задние # 0                                                           *");
-            Console.WriteLine("* Вариант #5                                                           *");
+            Console.WriteLine("* Задние # 2                                                         *");
+            Console.WriteLine("* Вариант #12                                                          *");
             Console.WriteLine("* Выполнил : Мороз А.Д | СМАРТб- 23-1                                  *");
             Console.WriteLine("************************************************************************");
             Console.WriteLine("* Условие:                                                             *");
-            Console.WriteLine("* Написать  программу, вычесляет выражение (1+2)*(1+9/3)               *");
+            Console.WriteLine("* Написать  программу,которая вычесляет объем параллелепипеда.         *");
             Console.WriteLine("* и печатает результат на экране.                                      *");
             Console.WriteLine("*                                                                      *");
             Console.WriteLine("************************************************************************");
-            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                     *");
-            Console.WriteLine("************************************************************************");
-            Console.WriteLine("* (1+2)*(1+9/3)                                                        *");
-            Console.WriteLine("************************************************************************");
-            Console.WriteLine("* РЕЗУЛЬТАТ:                                                           *");
-            Console.WriteLine("************************************************************************");
+            int x;
+            int y;
+            int z;
 
 
-            Console.WriteLine(ds.Calculate());
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение Y:");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение Z:");
+            z = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.CalculateParallelepipedVolume(x,y,z));
             Console.ReadLine();
+            Console.WriteLine("************************************************************************");
         }
     }
 }
---
+++ Tyuiu.MorozAD.Sprint1.Task2.V12.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.MorozAD.Sprint1.Task2.V12.Lib;
+
+namespace Tyuiu.MorozAD.Sprint1.Task2.V12.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 1;
+            int y = 2;
+            int z = 3;
+            var res = ds.CalculateParallelepipedVolume(x, y, z);
+            Assert.AreEqual(6, res);
+        }
+    }
+}
