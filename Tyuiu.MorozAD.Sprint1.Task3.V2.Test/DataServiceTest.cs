4c57b852b3adb47a5ea5cb31cbbb16a3b78895eb
Tyuiu.MorozAD@outlook.com <ggllaa5566q@gmail.com>
2023-10-15 22:13:42 +0500

    Task3

---
+++ Tyuiu.MorozAD.Sprint1.Task3.V2.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.MorozAD.Sprint1.Task3.V2.Lib;
+
+namespace Tyuiu.MorozAD.Sprint1.Task3.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            int x1 = 1;
+            double y = 2;
+            int y1 = 2;
+            var res = ds.PurchaseAmount(x, x1, y, y1);
+            Assert.AreEqual(5, res);
+        }
+    }
+}
---
+++ Tyuiu.MorozAD.Sprint1.Task3.V2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.MorozAD.Sprint1.Task3.V2.Lib;
+
+namespace Tyuiu.MorozAD.Sprint1.Task3.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил : Мороз А.Д |СМАРТб- 23-1";
+            Console.WriteLine("*********************************************************************************");
+            Console.WriteLine("* Спринт #1                                                                     *");
+            Console.WriteLine("* Тема: Базовые навыки работы С#                                                *");
+            Console.WriteLine("* Задние # 0                                                                    *");
+            Console.WriteLine("* Вариант #5                                                                    *");
+            Console.WriteLine("* Выполнил : Мороз А.Д | СМАРТб- 23-1                                           *");
+            Console.WriteLine("*********************************************************************************");
+            Console.WriteLine("* Условие:                                                                      *");
+            Console.WriteLine("* Написать  программу,которая стоимости покупки                                 *");
+            Console.WriteLine("*состоящей из нескольких тетрадей и карандашей, и печатает результат на экране. *");
+            Console.WriteLine("*                                                                               *");
+            Console.WriteLine("*********************************************************************************");
+
+            double x;
+            int x1;
+            double y;
+            int y1;
+
+
+            Console.WriteLine("Введите стоимость тетради :");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите колл-во тетрадей:");
+            x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите стоимость карандаша:");
+            y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите колл-во карандашей:");
+            y1 = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                    *");
+            Console.WriteLine("*********************************************************************************");
+            Console.WriteLine(ds.PurchaseAmount(x, x1, y, y1));
+            Console.ReadKey();
+        }
+    }
+}
