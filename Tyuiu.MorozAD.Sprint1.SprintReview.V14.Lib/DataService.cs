1f52ca93d047506da14a96b4d884c7c800f209d8
Tyuiu.MorozAD@outlook.com <ggllaa5566q@gmail.com>
2023-10-19 17:16:53 +0500

    Task7

--- Tyuiu.MorozAD.Sprint1.SprintReview.V14.Lib/DataService.cs
+++ Tyuiu.MorozAD.Sprint1.SprintReview.V14.Lib/DataService.cs
@@ -11,7 +11,7 @@ namespace Tyuiu.MorozAD.Sprint1.SprintReview.V14.Lib
     {
         public double Calculate(double x, double y)
         {
-            double res= (Math.Pow(2,-x)+(5*Math.Pow(x,2)/3*Math.Pow(x,3))-Math.Cos(Math.Pow(x,2))+Math.Sin(2*x*y));
+            double res = (Math.Pow(2,-x)+(5*Math.Pow(x,2)/3*Math.Pow(x,3))-Math.Cos(Math.Pow(x,2))+Math.Sin(2*x*y));
             return res;
         }
     }
